e3fa8774e8953e366ab539c03771c2fc8ab5f552
pablo <pmillana@uwaterloo.ca>
2021-08-18 16:32:42 -0400

    build all datasets

---
+++ src/build_db.sh
@@ -0,0 +1,11 @@
+python build_dp.py --data_path='../data/Vertebrata/Test Files' &&
+python build_dp.py --data_path='../data/Fish/Test Files/Actinopterygii' &&
+python build_dp.py --data_path='../data/Fish/Test Files/Neopterygii' &&
+python build_dp.py --data_path='../data/Fish/Test Files/Ostariophysi' &&
+python build_dp.py --data_path='../data/Fish/Test Files/Cypriniformes' &&
+python build_dp.py --data_path='../data/Fish/Test Files/Cyprinidae' &&
+python build_dp.py --data_path='../data/Bacteria/Test_Files' &&
+python build_dp.py --data_path='../data/Bacteria/Proteo_Test_Files' &&
+python build_dp.py --data_path='../data/Influenza-A/Test Files' &&
+python build_dp.py --data_path='../data/Dengue/Test Files' 
+python build_dp.py --data_path='../data/HBV/Test Files'
